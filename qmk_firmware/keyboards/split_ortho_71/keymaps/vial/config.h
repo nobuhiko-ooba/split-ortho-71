2d376668d1a0b3d7b96bde65b01e9458f195fe48
nobuhiko-ooba <ooba@bd.mbn.or.jp>
2026-03-29 15:28:04 +0900

    split-ortho-71-akdk.zipの解答面

---
+++ qmk_firmware/keyboards/split_ortho_71/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define EE_HANDS
+#define VIAL_KEYBOARD_UID {0xd9, 0xe2, 0xad, 0xc0, 0x36, 0x8b, 0x1f, 0x1b}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
